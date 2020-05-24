816e5a46723e4d97481bc96bbcbccdb45b054634
Luan Luz <luanbrunoalvesluz@gmail.com>
2020-05-23 23:49:12 -0300

    Adicionei o arquivo de fundamentos basicos

---
+++ 01-fundamentos/01-fundamentos-basicos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fundamentos básicos</title>
+</head>
+<body>
+    <?php
+        // Esse é meu arquivo de fundamentos de php
+
+        /*
+            Imprimindo meu nome e minha idade de duas
+            formas direfentes
+        */
+        echo "Meu nome é Luan Luz <br>";
+        print "Tenho 19 anos";
+    ?>
+</body>
+</html>
