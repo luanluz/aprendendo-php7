9952259d8114e7010fbc79bf2420af116a656be9
Luan Luz <luanbrunoalvesluz@gmail.com>
2020-05-27 23:11:33 -0300

    adicionei o operador de atribuicao

---
+++ 01-fundamentos/13-operadores-de-atribuicao.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+    ---------- Operadores de atribuição ----------
+*/
+
+// A variável vai ser definida utilizando 
+// o operador de atribuição e em seguida o valor
+// especificado
+$variavel = 10;
+echo $variavel;
+echo '<br>';
+
+// Podemos utilizar um operador aritmético e o operador
+// de atribuicao para simplificar a operação
+// como no exemplo abaixo
+$variavel += 10;
+echo $variavel;
+echo '<br>';
+
+// É válido para todas as operações aritméticas
+$variavel -= 10;
+echo $variavel;
+echo '<br>';
+
+$variavel *= 10;
+echo $variavel;
+echo '<br>';
+
+$variavel /= 10;
+echo $variavel;
+echo '<br>';
+
+$variavel %= 10;
+echo $variavel;
+echo '<br>';
