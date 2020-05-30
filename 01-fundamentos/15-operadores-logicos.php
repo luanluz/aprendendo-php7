29007a1099e39f884e36aa617dfefdfb8d3ddb85
Luan Luz <luanbrunoalvesluz@gmail.com>
2020-05-29 23:43:38 -0300

    arrumei o arquivo de operadores logicos. alguns outputs e comentarios estavam errados

--- 01-fundamentos/15-operadores-logicos.php
+++ 01-fundamentos/15-operadores-logicos.php
@@ -8,34 +8,33 @@ $idade = 19;
 $nome = "Luan";
 
 // E
-// &&
 // Retorna verdadeiro somente se as duas expressões
 // forem verdadeiras
-echo $idade == 19 && $nome == "Luan" ? "Verdadeiro" : "Falso";
+// &&
+echo $idade == 19 && $nome == "Luan" ? "Verdadeiro <br>" : "Falso <br>";
 // and
-echo $idade == 19 and $nome == "Bruno" ? "Verdadeiro" : "Falso";
-
+echo $idade == 19 and $nome == "Bruno" ? "Verdadeiro <br>" : "Falso <br>";
+echo '<br><br>';
 
 // Ou
 // Retorna verdadeiro se uma ou outra expressão ou as duas
 // forem verdadeiras
-// &&
-echo $idade == 19 || $nome == "Bruno" ? "Verdadeiro" : "Falso";
-// and
-echo $idade == 20 or $nome == "Luan" ? "Verdadeiro" : "Falso";
-
+// ||
+echo $idade == 19 || $nome == "Bruno" ? "Verdadeiro <br>" : "Falso <br>";
+// or
+echo $idade == 20 or $nome == "Luan" ? "Verdadeiro <br>" : "Falso <br>";
+echo '<br><br>';
 
 // Ou exclusivo
 // xor
 // Retorna verdadeiro somente se uma das expressões for
 // verdadeiras
-echo $idade == 19 xor $nome == "Bruno" ? "Verdadeiro" : "Falso";
-// and
-echo $idade == 19 xor $nome == "Luan" ? "Verdadeiro" : "Falso";
+echo var_dump($idade == 19 xor $nome == "Bruno") .'<br>';
+echo var_dump($idade == 19 xor $nome == "Luan") .'<br>';
+echo '<br><br>';
 
 // Negação
 // Retorna verdadeiro se a expressão for false e vice e versa
 // !
-echo !($idade == 19) && $nome == "Bruno" ? "Verdadeiro" : "Falso";
-// and
-echo !($idade == 19) and $nome == "Luan" ? "Verdadeiro" : "Falso";
+echo !($idade == 19) ? "Verdadeiro <br>" : "Falso <br>";
+echo !($idade != 19) ? "Verdadeiro <br>" : "Falso <br>";
