e5163312c10a3c12e64655f848133b0cee0b9f7e
Luan Luz <luanbrunoalvesluz@gmail.com>
2020-06-23 11:54:15 -0300

    adicionado comentários ao envio de arquivos

--- 03-upload/index.php
+++ 03-upload/index.php
@@ -9,24 +9,36 @@
 
     <?php
         if(isset($_POST['enviar-formulario'])):
+            // Tipos de arquivos que serão permitidos
             $formatosPermitidos = array('png', 'jpg', 'jpeg', 'gif');
 
             // var_dump($_FILES);
-            $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
 
+            // Percorrendo o array de arquivos
             for($i = 0; $i < count($_FILES['arquivos']['name']); $i++):
+                // Obtendo a tipo do arquivo do arquivo enviado
                 $extensao = pathinfo($_FILES['arquivos']['name'][$i], PATHINFO_EXTENSION);
+
+                // Se o arquivo enviado for do tipo de arquivo autorizado
+                // salvará o arquivo 
                 if(in_array($extensao, $formatosPermitidos)):
+                    // Definindo o diretório onde salvará os arquivos
                     $dir = "arquivos/";
+                    // Definindo um nome temporário para o arquivo
                     $tmp = $_FILES['arquivos']['tmp_name'][$i];
+                    // Definindo um novo nome único para o arquivo
                     $novoNome = uniqid().".$extensao";
 
+                    // Movendo o arquivo para o diretório com o novo nome
                     if(move_uploaded_file($tmp, $dir.$novoNome)):
+                        // Imprimindo a mensagem de sucesso
                         echo "Upload feito com sucesso para $dir$novoNome! <br>";
                     else:
+                        // Imprimindo mensagem de erro
                         echo "Erro ao enviar o arquivo $tmp. <br>";
                     endif;
                 else:
+                    // Informando que o arquivo não é aceito
                     echo "$extensao não é permitido! <br>";
                 endif;
             endfor;
